c7698444df8200abb356009af59baf2569ec8f63
Noureldine adel <noureldine.adel22@gmail.com>
2020-07-20 15:51:31 +0200

    well then...the site is done :)

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ AOS.init({
 	duration: 1000,
 });
 new Waypoint({
-	element: document.getElementById("square"),
+	element: document.getElementById("risk"),
 	handler: function (direction) {
 		document.querySelector("header").classList.toggle("light");
 		document.querySelector(".normal-logo").src =
@@ -25,7 +25,22 @@ new Waypoint({
 				? "./dist/images/Logo-white.png"
 				: "./dist/images/Logo.png";
 	},
+	offset: 200,
 });
+function toggleScrolling(bool) {
+	document.querySelector("html").style.overflowY = bool ? "hidden" : "auto";
+	document.querySelector("body").style.overflowY = bool ? "hidden" : "auto";
+}
+function toggleNav() {
+	document.querySelector(".hamburger").classList.toggle("is-active");
+	document.querySelector(".nav-page").classList.toggle("open");
+	toggleScrolling(
+		document.querySelector(".hamburger").classList.contains("is-active")
+	);
+}
 document.querySelector(".hamburger").addEventListener("click", function () {
-	this.classList.toggle("is-active");
+	toggleNav();
 });
+document
+	.querySelectorAll(".nav-page a")
+	.forEach((e) => e.addEventListener("click", toggleNav));
